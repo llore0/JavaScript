4e48b027c0aef670ed08fa83c23096cd4bc46e05
lore <lorenzo.toledo789@gmail.com>
2023-06-12 18:40:01 -0300

    js

--- Exercícios/ex.004/script.js
+++ Exercícios/ex.004/script.js
@@ -1,3 +1,3 @@
 var nome = window.prompt('Qual seu nome?')
 document.write(`Olá, <strong>${nome}</strong>! Seu nome contém ${nome.length} letras <br>`)
-document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}, e em minúsculo é ${nome.toLowerCase}`)
+document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}, e em minúsculo é ${nome.toLowerCase()}`)
---
+++ Exercícios/ex.005/script.js
@@ -0,0 +1,11 @@
+ /* Aritméticos
+    -----------
+    5 + 2 = 7
+    5 - 2 = 3
+    5 * 2 = 10
+    5 / 2 = 2.5
+    5 % 2 = 1 - Divide e pega o resto
+    5 ** 2 = 25 - elevado
+ */
+
+    
