b39d02f74527ee67eee114d80eaa96dc92105506
lore <lorenzo.toledo789@gmail.com>
2023-07-26 08:51:15 -0300

    estudando js
    addEventListerner + aero function

---
+++ CFBcursos/DOM/5 - addEventListener/1/1.js
@@ -0,0 +1,16 @@
+// const HTML = document.querySelector('#c0');
+
+// const clicar = () => {
+//     alert('clicou :D')
+// }
+
+// // HTML.addEventListener('click', clicar)
+
+// // HTML.addEventListener('click', ()=>{
+// //     clicar()
+// // })
+
+// HTML.addEventListener('click', (evt)=>{
+//     const el = evt.target
+//     el.classList.add('destaque')
+// })
