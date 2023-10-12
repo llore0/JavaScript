bbb9f88ca0fe4ec17078bb741834ebaaddd0b5da
lore <lorenzo.toledo789@gmail.com>
2023-10-12 15:25:30 -0300

    snake game

---
+++ CFBcursos/DOM/23 - Biblioteca Math/2/script.js
@@ -0,0 +1,13 @@
+const pupila_e = document.getElementById("pupila_e")
+
+let posX_mouse = 0
+let posY_mouse = 0
+// window.addEventListener('mousemove', (evt) => {
+//     posX_mouse = evt.clientX
+//     posY_mouse = evt.clientY
+//     pupilas.forEach ((o) => {
+//         // o.style.transform = 'rotate(90deg) translate(50px, 50px);'
+//     })
+// })
+
+pupila_e.style.transform = 'rotate(90deg) translate(50px, 50px);'
